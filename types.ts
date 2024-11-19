0aab97d4df87a368c80f6bf6db0b9ebfd2791516
Jorge-Sanchez-dev <jorgesl099@gmail.com>
2024-11-19 13:41:37 +0100

    Prueba 2

--- types.ts
+++ types.ts
@@ -2,16 +2,32 @@ import { ObjectId, type OptionalId} from "mongodb";
 
 export type Comportamiento = 'bueno' | 'malo';
 
+export type Coordenadas = {lat: number; log: number};
+
 //Ninyo
-export type NinyoModel = OptionalId<{
+export type NinoModel = OptionalId<{
   name: string; 
   comportamiento: Comportamiento;
   ubicacion: ObjectId;
 }>;
 
 //Lugar
-export type Lugar = OptionalId<{
+export type LugarModel = OptionalId<{
   nombre: string; 
-  coordenadas: { lat: number; log: number };
-  numNiniosBuenos: number;
+  coordenadas: Coordenadas;
+  numNinosBuenos: number;
 }>;
+
+export type Nino = {
+  id: string;
+  name: string; 
+  comportamiento: Comportamiento;
+  ubicacion: ObjectId;
+}
+
+export type Lugar = {
+  id: string;
+  nombre: string; 
+  coordenadas: Coordenadas;
+  numNinosBuenos: number;
+}
