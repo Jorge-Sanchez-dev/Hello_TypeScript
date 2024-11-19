6cc7743aaa266cd7adcbbd4558f750e85d40a14e
Jorge-Sanchez-dev <jorgesl099@gmail.com>
2024-11-19 10:15:21 +0100

    Primera entrega

--- main.ts
+++ main.ts
@@ -12,3 +12,20 @@ const client = new MongoClient(MONGO_URL);
 await client.connect();
 console.info("Connected to MongoDB");
 
+const db = client.db("agenda");
+
+const handler = async (req: Request): Promise<Response> => {
+  const method = req.method;
+  const url = new URL(req.url);
+  const path = url.pathname;
+
+  if (method === "GET") { //leer datos
+  } else if (method === "POST") { // aniadir datos
+  } else if (method === "PUT") { // actualizar datos
+  } else if (method === "DELETE") {//Eliminar datos
+  }
+
+  return new Response("endpoint not found", { status: 404 });
+};
+
+Deno.serve({ port: 3000 }, handler);
